6ec6aaa1ded5608d9cd557f1dee2e08cdd0b94ff
Ivan Krasin <krasin@robodone.com>
2017-06-02 11:39:41 -0700

    Add cache-defeating query value when fetching a manifest.
    Also, implement autoupdate.NeedsUpdate.

--- pkg/autoupdate/manifest.go
+++ pkg/autoupdate/manifest.go
@@ -6,24 +6,50 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 const ProdManifestURL = "https://storage.googleapis.com/robosla-agent/robosla-agent.json"
 
 type Manifest struct {
-	ARM   string `json:"arm"`
-	AMD64 string `json:"amd64"`
+	Version string `json:"version"`
+	ARM     string `json:"arm"`
+	AMD64   string `json:"amd64"`
 }
 
-func FetchAndParseManifest(url string) (*Manifest, error) {
-	resp, err := http.Get(url)
+func (m *Manifest) NeedsUpdate(version string) bool {
+	if IsDevBuild(version) {
+		// We don't autoupdate dev builds.
+		return false
+	}
+	return version != m.Version
+}
+
+func IsDevBuild(version string) bool {
+	return version == "dev"
+}
+
+func FetchAndParseManifest(manifestURL string) (*Manifest, error) {
+	// First, we need to add &t=<something unique> to bypass server-side cache.
+	purl, err := url.Parse(manifestURL)
 	if err != nil {
-		return nil, fmt.Errorf("http.Get(%q): %v", url, err)
+		return nil, fmt.Errorf("url.Parse(%q): %v", manifestURL, err)
+	}
+	query := purl.Query()
+	query["t"] = []string{fmt.Sprintf("%d", time.Now().UnixNano())}
+	purl.RawQuery = query.Encode()
+	manifestURL = purl.String()
+
+	// Now, actually fetch the manifest
+	resp, err := http.Get(manifestURL)
+	if err != nil {
+		return nil, fmt.Errorf("http.Get(%q): %v", manifestURL, err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch manifest from %q: %v", url, err)
+		return nil, fmt.Errorf("failed to fetch manifest from %q: %v", manifestURL, err)
 	}
 	if resp.StatusCode != 200 {
 		log.Printf("unexpected HTTP status: %s %d. Trying to parse the manifest anyway.", resp.Status, resp.StatusCode)
--- tools/show-manifest/main.go
+++ tools/show-manifest/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/robodone/robosla-common/pkg/autoupdate"
 )
 
+var version = flag.String("version", "dev", "Version to test with")
+
 func main() {
+	flag.Parse()
+	fmt.Printf("Version: %s\n", *version)
+	fmt.Printf("IsDevBuild: %v\n", autoupdate.IsDevBuild(*version))
 	m, err := autoupdate.FetchAndParseManifest(autoupdate.ProdManifestURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(m)
+	fmt.Printf("NeedsUpdate: %v\n", m.NeedsUpdate(*version))
 }
